0663c84e34972a704229f697e308eca6404e6b73
Bucky <buckybush@gmail.com>
2022-07-19 12:13:53 -0400

    updates to pull TermName

--- src/main-old.py
+++ src/main-old.py
@@ -1,19 +1,14 @@
-#Griffin Spalding County Schools
 import glob
 import io
 import os
 import zipfile
-import pandas as pd
-import psutil
 
 import requests
 
 from google.cloud import secretmanager, storage
 
 
-gcp_project_id = 'gscs-data'
-gcs_bucket = 'gscs-data-lake'
-
+gcp_project_id = os.environ.get('GCP_PROJECT')
 
 def get_secret():
     client = secretmanager.SecretManagerServiceClient()
@@ -23,9 +18,13 @@ def get_secret():
     return response.payload.data.decode("UTF-8")
 
 def main(request):
+
     nwea_api_url = 'https://api.mapnwea.org/services/reporting/dex'
-    nwea_username = 'data@gscs.org'
     nwea_password = get_secret()
+    nwea_username = os.environ.get('NWEA_USERNAME')
+    gcs_bucket = os.environ.get('GCS_BUCKET')
+
+    
     session = requests.Session()
     session.auth = (nwea_username, nwea_password)
     response = session.request('GET', nwea_api_url)
@@ -36,25 +35,13 @@ def main(request):
     zip = zipfile.ZipFile(io.BytesIO(response.content))
     zip.extractall('/tmp')
 
-    filename = 'ComboStudentAssessment.csv'
-
-    first_row = pd.read_csv(f'/tmp/{filename}', nrows=1)
-    if first_row.empty:
-        term_name = 'No Data'
-    else:
-        term_name = first_row['TermName'].values[0]
-    print(f'TermName = {term_name}')
-
-
     storage_client = storage.Client()
     bucket = storage_client.bucket(gcs_bucket)
-    remote_path = f'assessment/nwea_map/active/{term_name}/'
+
+    remote_path = blob = 'assessment/nwea_map/active/'
+
     for csv_path in glob.glob('/tmp/*.csv'):
         blob = bucket.blob(remote_path + os.path.basename(csv_path))
         blob.upload_from_filename(csv_path)
 
-    process = psutil.Process(os.getpid())
-    print("Process MB used: ", end='')
-    print(process.memory_info().rss / 1024 / 1024) # in MB
-
-    return f'Uploaded CSV file(s) to GCS'
+    return f'Uploaded CSV files to GCS'
